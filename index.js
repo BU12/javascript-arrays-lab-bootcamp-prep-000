a1f7abd2fc5b70486eb4b830f005d067a32b1bbf
BU12 <williampeterson23@gmail.com>
2017-10-04 02:17:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten(array, name) = [...kittens]
+  kittens.append(name);
+  return kittens
+
+var destructivelyPrependKitten(name) = [...kittens]
+  kittens.unshift(name);
+  return kittens
+
+var destructivelyRemoveLastKitten() = [...kittens]
+var destructivelyRemoveFirstKitten() = [...kittens]
+var appendKitten(name) = [...kittens]
+var prependKitten(name) = [...kittens]
+var removeLastKitten(name) = [...kittens]
+var removeFirstKitten(name) = [...kittens]
